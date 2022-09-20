1c910b933db50eb9c1c3477eeff663b6e5094bd7
Armel CODJIA <codjiaulrich5@outlook.fr>
2022-09-20 16:55:21 +0200

    corrected popup table issue

--- background.js
+++
@@ -1,4 +0,0 @@
-//To detect browserAction
-chrome.action.onClicked.addListener(function (tab) {
-    chrome.tabs.sendMessage(tab.id, { "message": "clicked_browser_action" });
-});
--- content-scripts.js
+++ content-scripts.js
@@ -1,20 +1,20 @@
 var initLog = "Activating 'PageReact'";
 console.log(initLog);
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.message === "clicked_browser_action") {
-    var currentHref = window.location.href;
-    fetchData(currentHref);
-  }
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+  let currentHref = tabs[0].url;
+  console.log(currentHref);
+  fetchData(currentHref);
 });
 
 async function fetchData(currentHref) {
   var matchOne = currentHref.match(
-    /(?:https?:\/\/(?:www|m|mbasic|business)\.(?:facebook|fb)\.com\/)(?:photo(?:\.php|s)|permalink\.php|video\.php|media|watch\/|questions|notes|[^\/]+\/(?:activity|posts|videos|photos))[\/?](?:fbid=|story_fbid=|id=|b=|v=|)(?|([0-9]+)|[^\/]+\/(\d+))/gm
+    /(?:https?:\/\/(?:www|m|mbasic|business)\.(?:facebook|fb)\.com\/)(?:photo(?:\.php|s)|permalink\.php|video\.php|media|watch\/|questions|notes|[^\/]+\/(?:activity|posts|videos|photos))[\/?](?:fbid=|story_fbid=|id=|b=|v=|)(([0-9]+)|[^\/]+\/(\d+))/
   );
   if (matchOne) {
     const res = await fetch(currentHref);
     const record = await res.json();
+    console.log(record);
     const likes = record.data[0].data;
     for (var i = 0; i < likes.length; i++) {
       var no = document.createElement("td");
@@ -23,9 +23,11 @@ async function fetchData(currentHref) {
       var uid = document.createElement("td");
       var uid_text = document.createTextNode(parseInt(likes[i].id));
       uid.appendChild(uid_text);
+      var tr = document.createElement("tr");
+      tr.appendChild(no);
+      tr.appendChild(uid);
       var element = document.getElementsById("list_of_uid");
-      element.appendChild(no);
-      element.appendChild(uid);
+      element.appendChild(tr);
     }
   }
 }
